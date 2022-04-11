a27c57a16781bc2eb81c147905c833ffc2326d4b
anon <anon@non.com>
2022-04-11 12:50:57 +0100

    track vault xtokensharevalue

--- src/mappings/utils/constants.ts
+++ src/mappings/utils/constants.ts
@@ -24,6 +24,16 @@ export const ARBITRUM_ONE_NFTX_VAULT_FACTORY = Address.fromString(
   "0xe01Cf5099e700c282A56E815ABd0C4948298Afae"
 );
 
+export const MAINNET_NFTX_INVENTORY_STAKING = Address.fromString(
+  "0x3E135c3E981fAe3383A5aE0d323860a34CfAB893"
+);
+export const RINKEBY_NFTX_INVENTORY_STAKING = Address.fromString(
+  "0x05aD54B40e3be8252CB257f77d9301E9CB1A9470"
+);
+export const ARBITRUM_ONE_NFTX_INVENTORY_STAKING = Address.fromString(
+  "0x64029E2da85B1d53815d111FEd15609034E5D557"
+);
+
 export const MAINNET_WETH = Address.fromString(
   "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
 );
@@ -41,6 +51,13 @@ export function NFTX_VAULT_FACTORY(network: string): Address {
   else if (network === "rinkeby") return RINKEBY_NFTX_VAULT_FACTORY;
   else if (network === "arbitrum-one") return ARBITRUM_ONE_NFTX_VAULT_FACTORY;
   return MAINNET_NFTX_VAULT_FACTORY;
+}
+
+export function NFTX_INVENTORY_STAKING(network: string): Address {
+  if (network === "mainnet") return MAINNET_NFTX_INVENTORY_STAKING;
+  else if (network === "rinkeby") return RINKEBY_NFTX_INVENTORY_STAKING;
+  else if (network === "arbitrum-one") return ARBITRUM_ONE_NFTX_INVENTORY_STAKING;
+  return MAINNET_NFTX_INVENTORY_STAKING;
 }
 
 export function WETH(network: string): Address {
